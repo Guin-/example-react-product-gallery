111d4bb3238711f3efc0150848885d0c1be77bce
Angelika Jarosz <angelikajarosz@gmail.com>
2019-07-05 13:01:45 -0400

    product cards content and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ const Header = ({setSearchText}) => {
   return (
     <section className="header">
       <div className="store-title">
-        <h1>Robots & Kittens</h1>
+        <h1>
+          Robots &<br />Kittens
+        </h1>
       </div>
       <Search setSearchText={setSearchText} />
     </section>
